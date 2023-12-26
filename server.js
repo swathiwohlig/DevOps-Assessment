235bfecf950563e1048f26b0326efdfff08d7f1b
swathiwohlig <swathi.cherala@wohlig.com>
2023-12-26 12:08:36 +0530

    testing

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ const HOST = '0.0.0.0';
 // App
 const app = express();
 app.get('/', (req, res) => {
-  res.send('Hello World Image');
+  res.send('Hello World Image New');
 });
 
 app.listen(PORT, HOST, () => {
